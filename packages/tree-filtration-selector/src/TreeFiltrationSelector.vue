68c396ba1568c093158d7f602948167df295d6b7
xuyuxuan_p <xu_yuxuan@139.com>
2021-03-10 17:22:06 +0800

    fix(treefiltrationselector): remove cosole.log

--- packages/tree-filtration-selector/src/TreeFiltrationSelector.vue
+++ packages/tree-filtration-selector/src/TreeFiltrationSelector.vue
@@ -156,14 +156,6 @@
               ])
             );
           }
-          console.log(
-            this.value,
-            node.id,
-            this.value
-              .map((item) => item.id)
-              .join(",")
-              .indexOf(node.id) === -1
-          );
         } else {
           // 开启单选 - 逻辑
           this.$emit("input", [node.id]);
